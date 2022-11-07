5439a09d45e57f95f17caf5ac607e1d419354af5
Nikolay Amiantov <ab@fmap.me>
2022-11-07 16:51:30 +0700

    mix-configure-hook: deduplicate dependencies

--- pkgs/development/beam-modules/mix-configure-hook.sh
+++ pkgs/development/beam-modules/mix-configure-hook.sh
@@ -3,16 +3,20 @@
 # since Elixir 1.12.2 elixir does not look into ERL_LIBS for
 # elixir depencencies anymore, so those have to be symlinked to the _build directory
 mkdir -p _build/"$MIX_ENV"/lib
+deps=()
 while read -r -d ':' lib; do
     for dir in "$lib"/*; do
-    # Strip version number for directory name if it exists, so naming of
-    # all libs matches what mix's expectation.
-    dest=$(basename "$dir" | cut -d '-' -f1)
-    build_dir="_build/$MIX_ENV/lib/$dest"
-    ((MIX_DEBUG == 1)) && echo "Linking $dir to $build_dir"
-    # Symlink libs to _build so that mix can find them when compiling.
-    # This is what allows mix to compile the package without searching
-    # for dependencies over the network.
-    ln -s "$dir" "$build_dir"
+        # Strip version number for directory name if it exists, so naming of
+        # all libs matches what mix's expectation.
+        dest=$(basename "$dir" | cut -d '-' -f1)
+        if ! [[ " ${deps[*]} " =~ " $dest " ]]; then
+            build_dir="_build/$MIX_ENV/lib/$dest"
+            ((MIX_DEBUG == 1)) && echo "Linking $dir to $build_dir"
+            # Symlink libs to _build so that mix can find them when compiling.
+            # This is what allows mix to compile the package without searching
+            # for dependencies over the network.
+            ln -s "$dir" "$build_dir"
+            deps+=("$dest")
+        fi
     done
 done <<< "$ERL_LIBS:"
