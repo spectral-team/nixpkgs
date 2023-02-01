f247577ee9e19426442b5fd5983a197289f08afd
Nikolay Amiantov <ab@fmap.me>
2023-02-01 14:16:50 +0700

    stage-1 module: add preModuleCommands

--- nixos/modules/system/boot/stage-1-init.sh
+++ nixos/modules/system/boot/stage-1-init.sh
@@ -259,6 +259,7 @@ done
 
 # Load the required kernel modules.
 echo @extraUtils@/bin/modprobe > /proc/sys/kernel/modprobe
+@preModuleCommands@
 for i in @kernelModules@; do
     info "loading module $(basename $i)..."
     modprobe $i
--- nixos/modules/system/boot/stage-1.nix
+++ nixos/modules/system/boot/stage-1.nix
@@ -316,7 +316,7 @@ let
     inherit (config.system.build) earlyMountScript;
 
     inherit (config.boot.initrd) checkJournalingFS verbose
-      preLVMCommands preDeviceCommands postDeviceCommands postResumeCommands postMountCommands preFailCommands kernelModules;
+      preModuleCommands preLVMCommands preDeviceCommands postDeviceCommands postResumeCommands postMountCommands preFailCommands kernelModules;
 
     resumeDevices = map (sd: if sd ? device then sd.device else "/dev/disk/by-label/${sd.label}")
                     (filter (sd: hasPrefix "/dev/" sd.device && !sd.randomEncryption.enable
@@ -497,6 +497,14 @@ in
       type = types.bool;
       description = lib.mdDoc ''
         Whether to run {command}`fsck` on journaling filesystems such as ext3.
+      '';
+    };
+
+    boot.initrd.preModuleCommands = mkOption {
+      default = "";
+      type = types.lines;
+      description = lib.mdDoc ''
+        Shell commands to be executed immediately before loading kernel modules.
       '';
     };
 
