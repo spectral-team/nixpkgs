3a05be6a1403abcd6fabe9dbdf67ae7cb02d4e00
Rvfg <i@rvf6.com>
2023-10-25 10:50:03 +0800

    nixos-rebuild: use --pipe instead of --pty in systemd-run

--- pkgs/os-specific/linux/nixos-rebuild/nixos-rebuild.sh
+++ pkgs/os-specific/linux/nixos-rebuild/nixos-rebuild.sh
@@ -662,7 +662,7 @@ if [[ "$action" = switch || "$action" = boot || "$action" = test || "$action" = 
         "-E" "NIXOS_INSTALL_BOOTLOADER"
         "--collect"
         "--no-ask-password"
-        "--pty"
+        "--pipe"
         "--quiet"
         "--same-dir"
         "--service-type=exec"
