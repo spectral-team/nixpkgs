f6a28b5d4cc4a247ef164c22d7b948b0a384bed1
Nikolay Amiantov <ab@fmap.me>
2023-11-22 18:29:51 +0700

    vector: ignore warnings

--- pkgs/tools/misc/vector/default.nix
+++ pkgs/tools/misc/vector/default.nix
@@ -115,6 +115,10 @@ rustPlatform.buildRustPackage {
   postPatch = ''
     substituteInPlace ./src/dns.rs \
       --replace "#[tokio::test]" ""
+  ''
+  # Don't fail on compilation warnings; this reduces flakiness when updating rustc.
+  + ''
+    sed -i '/^#!\[deny/d' src/lib.rs
   '';
 
   passthru = {
